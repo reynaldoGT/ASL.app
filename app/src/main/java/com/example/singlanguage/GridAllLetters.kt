6eb86a52876e630a0556d64d041c57b7515aaf1d
LarzlRael <46308154+LarzlRael@users.noreply.github.com>
2021-03-20 17:31:44 -0400

    added the letter in it class

--- app/src/main/java/com/example/singlanguage/GridAllLetters.kt
+++ app/src/main/java/com/example/singlanguage/GridAllLetters.kt
@@ -3,7 +3,6 @@ package com.example.singlanguage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class GridAllLetters : AppCompatActivity() {
@@ -17,85 +16,15 @@ class GridAllLetters : AppCompatActivity() {
 
         setContentView(R.layout.activity_grid_all_letters)
 
-        val lettersArray = ArrayList<Letter>()
-
-
-        lettersArray?.add(Letter("a", R.drawable.ic_a_letter))
-        lettersArray?.add(Letter("b", R.drawable.ic_b_letter))
-        lettersArray?.add(Letter("c", R.drawable.ic_c_letter))
-        lettersArray?.add(Letter("d", R.drawable.ic_d_letter))
-        lettersArray?.add(Letter("e", R.drawable.ic_e_letter))
-        lettersArray?.add(Letter("f", R.drawable.ic_f_letter))
-        lettersArray?.add(Letter("g", R.drawable.ic_g_letter))
-        lettersArray?.add(Letter("h", R.drawable.ic_h_letter))
-        lettersArray?.add(Letter("i", R.drawable.ic_i_letter))
-        lettersArray?.add(Letter("j", R.drawable.ic_j_letter))
-        lettersArray?.add(Letter("k", R.drawable.ic_k_letter))
-        lettersArray?.add(Letter("l", R.drawable.ic_l_letter))
-        lettersArray?.add(Letter("m", R.drawable.ic_m_letter))
-        lettersArray?.add(Letter("n", R.drawable.ic_n_letter))
-        lettersArray?.add(Letter("o", R.drawable.ic_o_letter))
-        lettersArray?.add(Letter("p", R.drawable.ic_p_letter))
-        lettersArray?.add(Letter("q", R.drawable.ic_q_letter))
-        lettersArray?.add(Letter("r", R.drawable.ic_r_letter))
-        lettersArray?.add(Letter("s", R.drawable.ic_s_letter))
-        lettersArray?.add(Letter("t", R.drawable.ic_t_letter))
-        lettersArray?.add(Letter("u", R.drawable.ic_u_letter))
-        lettersArray?.add(Letter("v", R.drawable.ic_v_letter))
-        lettersArray?.add(Letter("w", R.drawable.ic_w_letter))
-        lettersArray?.add(Letter("x", R.drawable.ic_x_letter))
-        lettersArray?.add(Letter("y", R.drawable.ic_y_letter))
-        lettersArray?.add(Letter("z", R.drawable.ic_z_letter))
-        
-        lettersArray?.add(Letter("a", R.drawable.ic_a_only_sing))
-        lettersArray?.add(Letter("b", R.drawable.ic_b_only_sing))
-        lettersArray?.add(Letter("c", R.drawable.ic_c_only_sing))
-        lettersArray?.add(Letter("d", R.drawable.ic_d_only_sing))
-        lettersArray?.add(Letter("e", R.drawable.ic_e_only_sing))
-        lettersArray?.add(Letter("f", R.drawable.ic_f_only_sing))
-        lettersArray?.add(Letter("g", R.drawable.ic_g_only_sing))
-        lettersArray?.add(Letter("h", R.drawable.ic_h_only_sing))
-        lettersArray?.add(Letter("i", R.drawable.ic_i_only_sing))
-        lettersArray?.add(Letter("j", R.drawable.ic_j_only_sing))
-        lettersArray?.add(Letter("k", R.drawable.ic_k_only_sing))
-        lettersArray?.add(Letter("l", R.drawable.ic_l_only_sing))
-        lettersArray?.add(Letter("m", R.drawable.ic_m_only_sing))
-        lettersArray?.add(Letter("n", R.drawable.ic_n_only_sing))
-        lettersArray?.add(Letter("ñ", R.drawable.ic_nn_letter))
-        lettersArray?.add(Letter("o", R.drawable.ic_o_only_sing))
-        lettersArray?.add(Letter("p", R.drawable.ic_p_only_sing))
-        lettersArray?.add(Letter("q", R.drawable.ic_q_only_sing))
-        lettersArray?.add(Letter("r", R.drawable.ic_r_only_sing))
-        lettersArray?.add(Letter("s", R.drawable.ic_s_only_sing))
-        lettersArray?.add(Letter("t", R.drawable.ic_t_only_sing))
-        lettersArray?.add(Letter("u", R.drawable.ic_u_only_sing))
-        lettersArray?.add(Letter("v", R.drawable.ic_v_only_sing))
-        lettersArray?.add(Letter("w", R.drawable.ic_w_only_sing))
-        lettersArray?.add(Letter("x", R.drawable.ic_x_only_sing))
-        lettersArray?.add(Letter("y", R.drawable.ic_y_only_sing))
-        lettersArray?.add(Letter("z", R.drawable.ic_z_only_sing))
-
-        //?  numbers sings
-
-        lettersArray?.add(Letter("0", R.drawable.ic_0_number))
-        lettersArray?.add(Letter("1", R.drawable.ic_1_number))
-        lettersArray?.add(Letter("2", R.drawable.ic_2_number))
-        lettersArray?.add(Letter("3", R.drawable.ic_3_number))
-        lettersArray?.add(Letter("4", R.drawable.ic_4_number))
-        lettersArray?.add(Letter("5", R.drawable.ic_5_number))
-        lettersArray?.add(Letter("6", R.drawable.ic_6_number))
-        lettersArray?.add(Letter("7", R.drawable.ic_7_number))
-        lettersArray?.add(Letter("8", R.drawable.ic_8_number))
-        lettersArray?.add(Letter("9", R.drawable.ic_9_number))
-        lettersArray?.add(Letter("10", R.drawable.ic_10_number))
-
+        val shared = Shared()
+        val lettersArray = shared.getOnlyLettersArray()
 
 
         lista = findViewById(R.id.gridList)
 
         lista?.setHasFixedSize(true)
 
-        layoutManager = GridLayoutManager(this,2)
+        layoutManager = GridLayoutManager(this, 2)
         lista?.layoutManager = layoutManager
 
         adaptador = AdapterLetters(lettersArray)
--- app/src/main/java/com/example/singlanguage/MainActivity.kt
+++ app/src/main/java/com/example/singlanguage/MainActivity.kt
@@ -25,87 +25,18 @@ class MainActivity : AppCompatActivity() {
     var edSendMessage: EditText? = null
     var textViewMessage: TextView? = null
 
-
-    var lettersArray: ArrayList<Letter>? = null
-//    private var arrayLetters: Letters? = null
+    var lettersArrays: ArrayList<Letter>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+//        Thread.sleep(2000)
+        setTheme(R.style.Theme_SingLanguage)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lettersArray = ArrayList<Letter>()
-
-
-//        lettersArray?.add(Letter("a", R.drawable.ic_a_letter))
-//        lettersArray?.add(Letter("b", R.drawable.ic_b_letter))
-//        lettersArray?.add(Letter("c", R.drawable.ic_c_letter))
-//        lettersArray?.add(Letter("d", R.drawable.ic_d_letter))
-//        lettersArray?.add(Letter("e", R.drawable.ic_e_letter))
-//        lettersArray?.add(Letter("f", R.drawable.ic_f_letter))
-//        lettersArray?.add(Letter("g", R.drawable.ic_g_letter))
-//        lettersArray?.add(Letter("h", R.drawable.ic_h_letter))
-//        lettersArray?.add(Letter("i", R.drawable.ic_i_letter))
-//        lettersArray?.add(Letter("j", R.drawable.ic_j_letter))
-//        lettersArray?.add(Letter("k", R.drawable.ic_k_letter))
-//        lettersArray?.add(Letter("l", R.drawable.ic_l_letter))
-//        lettersArray?.add(Letter("m", R.drawable.ic_m_letter))
-//        lettersArray?.add(Letter("n", R.drawable.ic_n_letter))
-//        lettersArray?.add(Letter("o", R.drawable.ic_o_letter))
-//        lettersArray?.add(Letter("p", R.drawable.ic_p_letter))
-//        lettersArray?.add(Letter("q", R.drawable.ic_q_letter))
-//        lettersArray?.add(Letter("r", R.drawable.ic_r_letter))
-//        lettersArray?.add(Letter("s", R.drawable.ic_s_letter))
-//        lettersArray?.add(Letter("t", R.drawable.ic_t_letter))
-//        lettersArray?.add(Letter("u", R.drawable.ic_u_letter))
-//        lettersArray?.add(Letter("v", R.drawable.ic_v_letter))
-//        lettersArray?.add(Letter("w", R.drawable.ic_w_letter))
-//        lettersArray?.add(Letter("x", R.drawable.ic_x_letter))
-//        lettersArray?.add(Letter("y", R.drawable.ic_y_letter))
-//        lettersArray?.add(Letter("z", R.drawable.ic_z_letter))
-
-        // only sings
-        lettersArray?.add(Letter("a", R.drawable.ic_a_only_sing))
-        lettersArray?.add(Letter("b", R.drawable.ic_b_only_sing))
-        lettersArray?.add(Letter("c", R.drawable.ic_c_only_sing))
-        lettersArray?.add(Letter("d", R.drawable.ic_d_only_sing))
-        lettersArray?.add(Letter("e", R.drawable.ic_e_only_sing))
-        lettersArray?.add(Letter("f", R.drawable.ic_f_only_sing))
-        lettersArray?.add(Letter("g", R.drawable.ic_g_only_sing))
-        lettersArray?.add(Letter("h", R.drawable.ic_h_only_sing))
-        lettersArray?.add(Letter("i", R.drawable.ic_i_only_sing))
-        lettersArray?.add(Letter("j", R.drawable.ic_j_only_sing))
-        lettersArray?.add(Letter("k", R.drawable.ic_k_only_sing))
-        lettersArray?.add(Letter("l", R.drawable.ic_l_only_sing))
-        lettersArray?.add(Letter("m", R.drawable.ic_m_only_sing))
-        lettersArray?.add(Letter("n", R.drawable.ic_n_only_sing))
-        lettersArray?.add(Letter("ñ", R.drawable.ic_nn_letter))
-        lettersArray?.add(Letter("o", R.drawable.ic_o_only_sing))
-        lettersArray?.add(Letter("p", R.drawable.ic_p_only_sing))
-        lettersArray?.add(Letter("q", R.drawable.ic_q_only_sing))
-        lettersArray?.add(Letter("r", R.drawable.ic_r_only_sing))
-        lettersArray?.add(Letter("s", R.drawable.ic_s_only_sing))
-        lettersArray?.add(Letter("t", R.drawable.ic_t_only_sing))
-        lettersArray?.add(Letter("u", R.drawable.ic_u_only_sing))
-        lettersArray?.add(Letter("v", R.drawable.ic_v_only_sing))
-        lettersArray?.add(Letter("w", R.drawable.ic_w_only_sing))
-        lettersArray?.add(Letter("x", R.drawable.ic_x_only_sing))
-        lettersArray?.add(Letter("y", R.drawable.ic_y_only_sing))
-        lettersArray?.add(Letter("z", R.drawable.ic_z_only_sing))
-
-        //?  numbers
-        lettersArray?.add(Letter("1", R.drawable.ic_1_number))
-        lettersArray?.add(Letter("2", R.drawable.ic_2_number))
-        lettersArray?.add(Letter("3", R.drawable.ic_3_number))
-        lettersArray?.add(Letter("4", R.drawable.ic_4_number))
-        lettersArray?.add(Letter("5", R.drawable.ic_5_number))
-        lettersArray?.add(Letter("6", R.drawable.ic_6_number))
-        lettersArray?.add(Letter("7", R.drawable.ic_7_number))
-        lettersArray?.add(Letter("8", R.drawable.ic_8_number))
-        lettersArray?.add(Letter("9", R.drawable.ic_9_number))
-        lettersArray?.add(Letter("9", R.drawable.ic_9_number))
-        lettersArray?.add(Letter("10", R.drawable.ic_10_number))
-        lettersArray?.add(Letter("10", R.drawable.ic_10_number))
-        lettersArray?.add(Letter("0", R.drawable.ic_0_number))
+        val shared = Shared()
+        lettersArrays = shared.getLetterArray()
 
 
         imageview = findViewById(R.id.imagv)
@@ -138,13 +69,6 @@ class MainActivity : AppCompatActivity() {
 
         buttonSendMessage?.setOnClickListener {
 
-            // generate the message in array letters
-//            val newArray =
-//                generateSingLanguageMessage((edSendMessage?.text.toString()).trim().toLowerCase())
-
-            // show the message changing the main image view
-//            hideKeyboard()
-
             generateSingLanguageMessage(edSendMessage?.text.toString())
         }
         button?.setOnClickListener {
@@ -156,14 +80,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun changeImage() {
 
-        val randomLetter = (0..lettersArray!!.size - 1).random()
-        val newLetter: String = lettersArray!![randomLetter].letter
+        val randomLetter = (0 until lettersArrays!!.size).random()
+        val newLetter: String = lettersArrays!![randomLetter].letter
 //        Toast.makeText(this, newLetter, Toast.LENGTH_SHORT).show()
 
         imageview?.setImageDrawable(
             ContextCompat.getDrawable(
                 applicationContext, // Context
-                lettersArray!![randomLetter].image
+                lettersArrays!![randomLetter].image
             )
         )
     }
@@ -176,7 +100,7 @@ class MainActivity : AppCompatActivity() {
 
         hideKeyboard()
 
-        val cleanString = message.trim().toLowerCase()
+        val cleanString = message.trim().toLowerCase(Locale.ROOT)
 
         val arraySentenceSing = ArrayList<Letter>()
 //        val stringArray = message.toCharArray()
@@ -185,7 +109,7 @@ class MainActivity : AppCompatActivity() {
 
         for (i in stringArray) {
 
-            for (letterPosition in lettersArray!!) {
+            for (letterPosition in lettersArrays!!) {
                 if (letterPosition.letter == i.toString()) {
                     arraySentenceSing.add(letterPosition)
                 }
@@ -193,7 +117,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        showMessageWithSing(arraySentenceSing, timeTimer = 800)
+        showMessageWithSing(arraySentenceSing, timeTimer = 700)
     }
 
     private fun showMessageWithSing(sentenceInArrayImage: ArrayList<Letter>, timeTimer: Long) {
