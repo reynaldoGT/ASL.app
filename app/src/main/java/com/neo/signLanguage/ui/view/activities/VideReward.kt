e7e689cb0a44459cd966a3cd5fa79e5b08b2069c
Reynaldo GT <larz.dev@gmail.com>
2022-09-05 10:08:54 -0400

    added video reward and others changes

---
+++ app/src/main/java/com/neo/signLanguage/ui/view/activities/VideReward.kt
@@ -0,0 +1,11 @@
+package com.neo.signLanguage.ui.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideReward : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_vide_reward)
+  }
+}
